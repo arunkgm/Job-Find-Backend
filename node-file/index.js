9891ed134c3dc36a89b7085bcb1250fcbea3b90c
mathannarunkumar <92842755+mathankumaryesaian@users.noreply.github.com>
2022-09-05 12:58:56 +0530

    second commit

--- node-file/index.js
+++ node-file/index.js
@@ -25,7 +25,7 @@ mongoose.connect(
     "mongodb://localhost:27017/Data",
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
-        console.log("database started")
+        console.log("database started successfully")
     }
 
 );
